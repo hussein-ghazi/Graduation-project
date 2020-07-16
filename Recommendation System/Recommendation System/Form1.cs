b2e1c5911602145027d7ef8e0c4963c072f8e020
Zaid Kindman <38330943+zaid1997@users.noreply.github.com>
2020-07-16 09:38:59 +0300

    last modified

--- Recommendation System/Recommendation System/Form1.cs
+++ Recommendation System/Recommendation System/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 using RecommendationSystem;
 using RecommendationSystemFiles;
 using EvaluationModel;
